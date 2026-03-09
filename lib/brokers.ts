83df7c5567e2e3334e6e208f5a6967587d7c8454
Cromachain93521 <cromaart.official@gmail.com>
2026-03-09 22:41:31 +0700

    update

--- lib/brokers.ts
+++ lib/brokers.ts
@@ -25,7 +25,7 @@ export const brokers: Broker[] = [
   {
     name: "Exness",
     type: "auto",
-    ibLink: "https://one.exnessonelink.com/a/8cegzmlbpk",
+    ibLink: "https://one.exnessonelink.com/boarding/sign-up/a/fxpayout?lng=id",
     domain: "exness.com",
     instruments: [
       { name: "XAU/USD", rebate: 11.23 },
@@ -177,7 +177,7 @@ export const brokers: Broker[] = [
       { name: "AUD/USD", rebate: 3 }
     ]
   },
-
+ 
   {
     name: "Deriv",
     type: "volume",
